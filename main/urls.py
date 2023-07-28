dfd0aab3573749b815e21f19fe451cd218e07e8b
kanon chakma <kanonchakma@pop-os.localdomain>
2023-07-28 22:03:18 +0600

    add create-post

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home, sign_up
+from .views import create_post, home, sign_up
 
 urlpatterns = [
     path("", home, name="home"),
     path("home", home, name="home"),
     path("sign-up", sign_up, name="sign-up"),
+    path("create-post", create_post, name="create_post"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
-from .forms import RegistrationForm
+from .forms import PostCreationForm, RegistrationForm
 
 
+@login_required(login_url="/login")
 def home(request):
     return render(request, "main/home.html")
 
@@ -19,3 +21,17 @@ def sign_up(request):
         form = RegistrationForm()
 
     return render(request, "registration/sign_up.html", {"form": form})
+
+
+@login_required(login_url="/login")
+def create_post(request):
+    if request.method == "POST":
+        form = PostCreationForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("/home")
+    else:
+        form = PostCreationForm()
+    return render(request, "main/create_post.html", {"form": form})
